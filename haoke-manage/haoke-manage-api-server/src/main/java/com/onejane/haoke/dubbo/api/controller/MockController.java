76edd39fe045da65bad16e4480270160a4a7f61e
onejane <wangjian@jddmoto.com>
2020-02-07 10:41:47 +0800

    客户端首页伪mock服务

---
+++ haoke-manage/haoke-manage-api-server/src/main/java/com/onejane/haoke/dubbo/api/controller/MockController.java
@@ -0,0 +1,71 @@
+package com.onejane.haoke.dubbo.api.controller;
+
+import com.onejane.haoke.dubbo.api.config.MockConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("mock")
+@RestController
+@CrossOrigin
+public class MockController {
+
+    @Autowired
+    private MockConfig mockConfig;
+
+    @GetMapping("index/menu")
+    public String indexMenu(){
+        return this.mockConfig.getIndexMenu();
+    }
+
+    /**
+     * 首页资讯
+     * @return
+     */
+    @GetMapping("index/info")
+    public String indexInfo() {
+        return this.mockConfig.getIndexInfo();
+    }
+    /**
+     * 首页问答
+     * @return
+     */
+    @GetMapping("index/faq")
+    public String indexFaq() {
+        return this.mockConfig.getIndexFaq();
+    }
+    /**
+     * 首页房源信息
+     * @return
+     */
+    @GetMapping("index/house")
+    public String indexHouse() {
+        return this.mockConfig.getIndexHouse();
+    }
+
+    /**
+     * 查询资讯
+     *
+     * @param type
+     * @return
+     */
+    @GetMapping("infos/list")
+    public String infosList(@RequestParam("type")Integer type) {
+        switch (type){
+            case 1:
+                return this.mockConfig.getInfosList1();
+            case 2:
+                return this.mockConfig.getInfosList2();
+            case 3:
+                return this.mockConfig.getInfosList3();
+        }
+        return this.mockConfig.getInfosList1();
+    }
+    /**
+     * 我的中心
+     * @return
+     */
+    @GetMapping("my/info")
+    public String myInfo() {
+        return this.mockConfig.getMy();
+    }
+}
