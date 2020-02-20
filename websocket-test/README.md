71ab645264ff92a61ead027b22968d56de19aaf0
onejane <wangjian@jddmoto.com>
2020-02-20 10:32:23 +0800

    SpringBoot Websocket

--- websocket-test/README.md
+++ websocket-test/README.md
@@ -3,3 +3,8 @@ maven Plugins tomcat7 tomcat7:run
 http://www.websocket-test.com/
 ws://127.0.0.1:8082/websocket/1  
 http://127.0.0.1:8082/index.html  查看控制台
+# SpringBoot Websocket
+启动MyApplication 
+http://www.websocket-test.com/
+ws://127.0.0.1:8080/ws
+发送10
---
+++ websocket-test/src/main/java/com/onejane/websocket/spring/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.onejane.websocket.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private MyHandler myHandler;
+
+    @Autowired
+    private MyHandshakeInterceptor myHandshakeInterceptor;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(this.myHandler, "/ws")
+                .setAllowedOrigins("*") // 允许跨域
+                .addInterceptors(this.myHandshakeInterceptor);
+    }
+}
